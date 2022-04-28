13b298a00a34b09342db20697d16b982197b270f
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-04-28 16:02:25 -0400

    tests 1 through 7 complete

--- api/server.test.js
+++ api/server.test.js
@@ -16,26 +16,58 @@ afterAll(async () => {
   await db.destroy();
 });
 
-test("sanity check", () => {
+test("[1] sanity check", () => {
   expect(true).not.toBe(false);
 });
 
-test("make sure our environment is set correctly", () => {
+test("[2] make sure our environment is set correctly", () => {
   expect(process.env.NODE_ENV).toBe("testing");
 });
 
 describe("data access function are working properly", () => {
-  test.todo("getAll");
-  test.todo("getById");
-  test.todo("insert");
-  test.todo("remove");
-  test.todo("update");
+  test("[3] getAll", async () => {
+    const result = await Student.getAll();
+    expect(result.length).toBe(3);
+    expect(result[0]).toMatchObject({ student_name: "Bobby", student_grade: 75 });
+  });
+
+  test("[4] getById", async () => {
+    const result = await Student.getById(2);
+    expect(result).toBeDefined();
+    expect(result.student_name).toBe("Marry");
+  });
+
+  test("[5] insert", async () => {
+    let result = await Student.insert({ student_name: "Will", student_grade: 40 });
+    expect(result).toHaveProperty("student_id", 4);
+    result = await Student.getAll();
+    expect(result.length).toBe(4);
+  });
+
+  test("[6] remove", async () => {
+    let result = await Student.remove(1);
+    expect(result).toHaveProperty("student_name", "Bobby");
+    result = await Student.getAll();
+    expect(result).toHaveLength(2);
+    expect(result[1].student_id).toBe(3);
+  });
+
+  test("[7] update", async () => {
+    let result = await Student.update(3, { student_name: "Samantha" });
+    expect(result).toEqual({ student_id: 3, student_name: "Samantha", student_grade: 100 });
+    result = await Student.getAll();
+    expect(result).toHaveLength(3);
+  });
 });
 
 describe("HTTP API functions are working properly", () => {
-  test.todo("GET /api/students");
-  test.todo("GET /api/students/:id");
-  test.todo("POST /api/students");
-  test.todo("DELETE /api/students/:id");
-  test.todo("PUT /api/students/:id");
+  test("[8] GET /api/students", async () => {
+    const res = await request(server).get("/api/students");
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveLength(3);
+  });
+  test.todo("[9] GET /api/students/:id");
+  test.todo("[10] POST /api/students");
+  test.todo("[11] DELETE /api/students/:id");
+  test.todo("[12] PUT /api/students/:id");
 });
