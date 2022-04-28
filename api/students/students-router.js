08d29193f9104503538a8e96fa75a9473c65057b
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-04-28 15:29:05 -0400

    added router and data access functions

--- api/students/students-router.js
+++ api/students/students-router.js
@@ -1,7 +1,45 @@
 const router = require("express").Router();
 
+const Student = require("./students-model");
+
 router.get("/", (req, res, next) => {
-  res.json({ router: "up" });
+  Student.getAll()
+    .then((students) => {
+      res.json(students);
+    })
+    .catch((err) => next(err));
+});
+
+router.get("/:id", (req, res, next) => {
+  Student.getById(req.params.id)
+    .then((student) => {
+      res.json(student);
+    })
+    .catch((err) => next(err));
+});
+
+router.post("/", (req, res, next) => {
+  Student.insert(req.body)
+    .then((newStudent) => {
+      res.status(201).json(newStudent);
+    })
+    .catch((err) => next(err));
+});
+
+router.delete("/:id", (req, res, next) => {
+  Student.remove(req.params.id)
+    .then((deletedStudent) => {
+      res.json(deletedStudent);
+    })
+    .catch((err) => next(err));
+});
+
+router.put("/:id", (req, res, next) => {
+  Student.update(req.params.id, req.body)
+    .then((updatedStudent) => {
+      res.json(updatedStudent);
+    })
+    .catch((err) => next(err));
 });
 
 module.exports = router;
